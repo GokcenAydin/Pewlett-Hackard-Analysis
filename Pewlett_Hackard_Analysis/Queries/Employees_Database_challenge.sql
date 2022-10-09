886b3355881d2a57eca849bd50db8fb938e0e21a
GokcenAydin <104400293+GokcenAydin@users.noreply.github.com>
2022-10-08 21:55:11 -0400

    Module_7_Challenge_Assignment

---
+++ Pewlett_Hackard_Analysis/Queries/Employees_Database_challenge.sql
@@ -0,0 +1,45 @@
+-- Create a new file by selecting rows
+SELECT emp_no, first_name, last_name
+INTO employee_ret
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+
+-- Join new file into retirement_titles
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM titles as t
+INNER JOIN employee_ret as e
+ON (e.emp_no = t.emp_no)
+ORDER BY e.emp_no, t.emp_no ASC;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01' AND e.birth_date BETWEEN '1965-01-01' and '1965-12-31'
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
+
+
+
